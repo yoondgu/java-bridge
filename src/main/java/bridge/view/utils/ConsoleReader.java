581fa92d6f8042b9e8c134e963127c05da135240
yoondgu <doyou2ing@naver.com>
2022-11-20 16:55:35 +0900

    :bug:fix: ConsoleReader 정수 예외처리 코드 수정 #8
    - 기존의 정규 표현식 활용한 정수 검증코드로는 길이가 1인 문자열("a"..)을 검증할 수 없음을 발견.
    - 따라서 readLineAsInteger 메소드에서 검증 코드 대신 try/catch로 예외 처리하도록 수정함.
    - 추후 검증 로직을 분리하기 위해 다시 정규표현식 활용 방안 검토할 것.

--- src/main/java/bridge/view/utils/ConsoleReader.java
+++ src/main/java/bridge/view/utils/ConsoleReader.java
@@ -4,14 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleReader {
 
-    private static final String REGEX_INTEGER = "^[^0]\\d*";
-
     public static int readLineAsInteger() {
         String line = readLine();
-        if (line.matches(REGEX_INTEGER)) {
-            throw new IllegalArgumentException("입력 오류: 해당 입력값은 정수만 허용됩니다.");
+        // TODO 정규표현식 활용해서 검증로직 분리 검토(정규표현식 사용 시 문자열 길이 1인 경우 별도 검증 필요)
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("입력 오류: 해당 입력값은 정수값만 허용됩니다.");
         }
-        return Integer.parseInt(line);
     }
 
     public static String readLine() {
