c56b4397dec0da6fc378ccfb2501d16158f81620
yoondgu <doyou2ing@naver.com>
2022-11-20 22:10:43 +0900

    :sparkles:feat: 입력받은 재시도/종료 결과에 따라 게임을 재시도/종료하는 기능 구현 #7
    - GameController 코드 전반적으로 리팩토링 필요.
    - while 문 내의 break 지양하도록 리팩토링 필요.

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -15,7 +15,8 @@ public class GameController {
     private BridgeGame bridgeGame;
 
     public void run() {
-        System.out.println("다리의 길이를 입력해주세요.\n");
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+        System.out.println("다리의 길이를 입력해주세요.");
         int bridgeSize = askBridgeSize();
         bridgeGame = new BridgeGame(bridgeSize);
         MovingMap movingMap = null;
@@ -28,16 +29,17 @@ public class GameController {
             List<String> movingHistory = bridgeGame.getPlayerMovingHistory();
             movingMap = new MovingMap(new MovingMapGenerator(movingHistory, hasFailed));
             outputView.printMap(movingMap);
+
             if (hasFailed) {
-                // TODO 재시도/종료 여부에 따라 bridgeGame.retry() / stopMoving = true;
-                // TODO 재시도/종료 여부 입력받기
-                boolean toQuit = false;
+                System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+                boolean toQuit = !askGameCommand();
                 if (toQuit) {
                     break;
                 }
                 bridgeGame.retry();
             }
         }
+
         System.out.println("최종 게임 결과");
         outputView.printMap(movingMap);
         outputView.printResult(bridgeGame.hasPlayerFailed(), bridgeGame.getTrialCount());
@@ -58,6 +60,15 @@ public class GameController {
         } catch (IllegalArgumentException exception) {
             System.out.println("[ERROR] " + exception.getMessage());
             return askMoving();
+        }
+    }
+
+    private boolean askGameCommand() {
+        try {
+            return inputView.readGameCommand();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("[ERROR] " + exception.getMessage());
+            return askGameCommand();
         }
     }
 }
